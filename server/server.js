bf07aedeb6ad27f0524436a031b88d6c1105d94e
Bartek <brt0555@interia.eu>
2018-05-31 16:54:35 +0200

    Add POST /users and token method

--- server/server.js
+++ server/server.js
@@ -90,12 +90,26 @@ app.patch('/todos/:id', (req,res) => {
         if(!todo) {
             return res.status(404).send();
         }
-        console.log(todo);
         res.send({todo});
     }).catch(e => res.status(400).send());
-
-
 });
+
+
+app.post('/users', (req,res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    const user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
