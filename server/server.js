270ffeae5a7d8180bc3516e5eeb547cba8542f76
Bartek <brt0555@interia.eu>
2018-05-31 13:23:22 +0200

    delete todo call

--- server/server.js
+++ server/server.js
@@ -41,11 +41,30 @@ app.get('/todos/:id', (req,res) => {
 
     Todo.findById(id).then(todo => {
         if(!todo) {
-            return res.status(404).send({});
+            return res.status(404).send();
         }
 
         res.send({todo});
     }).catch(e => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove({_id: id}).then((todo) => {
+    if(!todo) {
+        return res.status(404).send();
+    }
+
+        res.status(200).send({todo});
+    })
+    .catch(e => {
         res.status(400).send();
     });
 });
