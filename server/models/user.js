55aada1673214cf1bc44d55ec5a718ca2ddc9354
Bartek <brt0555@interia.eu>
2018-05-31 19:21:24 +0200

    Add login call

--- server/models/user.js
+++ server/models/user.js
@@ -85,7 +85,22 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.token': token,
         'tokens.access': 'auth'
     })
+};
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+
+    return User.findOne({email}).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                res ? resolve(user): reject();
+            })
+        })
+    })
 }
 
 
--- server/server.js
+++ server/server.js
@@ -115,6 +115,20 @@ app.get('/users/me',authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch(e => res.status(400).send(e));
+});
+
 
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
